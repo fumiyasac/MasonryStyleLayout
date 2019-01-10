488062ba0c8947bb9287b3b939b8c4f43c43e74f
Fumiya Sakai <just1factory@gmail.com>
2019-01-10 18:52:34 +0900

    実装が必要そうな部分を取り急ぎ決める

---
+++ MasonryStyleLayout/View/PhotoGalleryCollectionViewLayout/PhotoGalleryCollectionViewLayout.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoGalleryCollectionViewLayout.swift
+//  MasonryStyleLayout
+//
+//  Created by 酒井文也 on 2019/01/10.
+//  Copyright © 2019 酒井文也. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// TODO: セクション付きのMasonryLayout用のクラスを実装する
