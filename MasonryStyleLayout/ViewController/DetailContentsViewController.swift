e237fa51c0b91ad0ed4f107935579d32ba0a1a90
Fumiya Sakai <just1factory@gmail.com>
2019-01-22 20:23:49 +0900

    詳細画面の追加

---
+++ MasonryStyleLayout/ViewController/DetailContentsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailContentsViewController.swift
+//  MasonryStyleLayout
+//
+//  Created by 酒井文也 on 2019/01/22.
+//  Copyright © 2019 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class DetailContentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
